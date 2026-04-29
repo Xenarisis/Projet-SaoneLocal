fb73afa4af90716b7588dd6c8c896f61e0fb04d3
xen <romainrobert.xen@gmail.com>
2026-04-29 10:44:08 +0200

    feat(migration): add migration files for tables in DB

---
+++ database/migrations/2026_04_21_080837_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->string('category');
+            $table->string('subcategory');
+            $table->integer('product_number');
+            $table->tinytext('description');
+            $table->integer('price');
+            $table->datetime('created_at');
+            $table->foreignId('opinion_id')->constrained('opinion')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
