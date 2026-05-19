1f562deb02c91315f3e913fd3af7f23271b9f64a
filouche972 <filouche39@gmail.com>
2026-05-19 15:17:13 +0200

    feat(seeders): Added User seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        User::create([
+            'email' => 'filouche39@gmail.com',
+            'firstname' => 'Félicien',
+            'lastname' => 'Nachon',
+            'username' => 'filouche',
+            'role' => 'admin',
+            'password' => Hash::make('admin123')
+        ]);
+
+        // User::factory()->count(50)->create();
+    }
+}
