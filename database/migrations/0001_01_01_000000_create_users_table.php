e1ce6e486427ce417318f47117f63dfd0c9e070b
filouche972 <filouche39@gmail.com>
2026-05-20 10:49:12 +0200

    feat(migrations): edited migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -5,10 +5,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         // Table Users
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -32,19 +30,18 @@ return new class extends Migration
         //     $table->timestamp('created_at')->nullable();
         // });
 
-        // Table Sessions
-        // Schema::create('sessions', function (Blueprint $table) {
-        //     $table->string('id')->primary();
-        //     $table->foreignId('user_id')->nullable()->index();
-        //     $table->string('ip_address', 45)->nullable();
-        //     $table->text('user_agent')->nullable();
-        //     $table->longText('payload');
-        //     $table->integer('last_activity')->index();
-        // });
+        // Table Sessions (Laravel need it)
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('users');
         // Schema::dropIfExists('password_reset_tokens');
         // Schema::dropIfExists('sessions');
