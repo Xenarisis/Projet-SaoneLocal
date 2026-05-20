f866d8812cfe632a7ae6369e18744d471e4c9ff3
filouche972 <filouche39@gmail.com>
2026-05-20 10:48:04 +0200

    feat(factory): added new factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,26 +10,26 @@ use Illuminate\Support\Str;
 /**
  * @extends Factory<User>
  */
-class UserFactory extends Factory
-{
+class UserFactory extends Factory {
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    // protected static ?string $password;
 
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
         return [
-            'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'username' => fake()->unique()->userName(),
+            'role' => 'user',
+            // 'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password')
         ];
     }
 
