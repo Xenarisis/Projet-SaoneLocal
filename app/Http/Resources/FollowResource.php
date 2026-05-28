2d8b382ac8a852235f79ed1e12dc7a6baeba1270
filouche <250682981+filouche-p@users.noreply.github.com>
2026-05-28 16:47:50 +0200

    feat(resource): added follox resource

---
+++ app/Http/Resources/FollowResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FollowResource extends JsonResource {
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array {
+        return [
+            'user_id'       => $this->user_id,
+            'producer_id'   => $this->producer_id
+        ];
+    }
+}
