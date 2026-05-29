da5e8e95567b47817991d8a4e45e59318001eee7
filouche <250682981+filouche-p@users.noreply.github.com>
2026-05-29 10:08:59 +0200

    feat(controller): edited follow controller

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -2,15 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Follow;
 use App\Models\Producer;
 use Illuminate\Http\Request;
-use app\Http\Resources\FollowResource;
-// use app\Http\Requests\CreateFollowRequest;
+use Illuminate\Support\Facades\Gate;
+use App\Http\Resources\FollowResource;
 
 class FollowController extends Controller {
     // Read
+    public function getFollowById(Follow $follow) {
+        Gate::authorize('view', $follow);
+
+        return new FollowResource($follow->load('user')->load('producer'));
+    }
+
+    public function getUserFollow(User $user) {
+        Gate::authorize('viewUserFollows', [Follow::class, $user]);
+
+        $follows = $user->follows()->with('producer')->get(); 
+    
+        return FollowResource::collection($follows);
+    }
+
+    public function getProducerFollowers(Producer $producer) {
+        Gate::authorize('viewProducerFollowers', [Follow::class, $producer]);
+
+        $followers = $producer->followers()->with('user')->get();
+
+        return FollowResource::collection($followers);
+    }
+
+    // Create
     public function createFollow(Request $request, Producer $producer) {
+        Gate::authorize('create', Follow::class);
+
         $user = $request->user();
 
         $follow = Follow::firstOrCreate([
@@ -27,8 +53,26 @@ class FollowController extends Controller {
         return (new FollowResource($follow))->additional(['message' => 'Follow ajouté avec succès.'])->response()->setStatusCode(201);
     }
 
-    public function deleteFollow(Request $request, Producer $producer) {
-        
-    }
+    // Update (no update needed in this controller)
 
+    // Delete
+    public function deleteFollow(Request $request, Producer $producer) {
+        $user = $request->user();
+
+        $follow = Follow::where('user_id', $user->id)->where('producer_id', $producer->id)->first();
+
+        if (!$follow) {
+            return response()->json([
+                'message' => 'Vous ne suivez pas ce producteur.'
+            ], 404);
+        }
+
+        Gate::authorize('delete', $follow);
+
+        $follow->delete();
+
+        return response()->json([
+            'message' => 'Abonnement supprimé avec succès.'
+        ], 204);
+    }
 }
