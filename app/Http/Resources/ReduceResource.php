f2638ef7377abe8e8f8eb0525c0142fa3889bbc7
filouche <250682981+filouche-p@users.noreply.github.com>
2026-06-15 22:23:09 +0200

    fix(resources): clean all resources code

--- app/Http/Resources/ReduceResource.php
+++ app/Http/Resources/ReduceResource.php
@@ -5,18 +5,19 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ReduceResource extends JsonResource
-{
+class ReduceResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
-    {
+    public function toArray(Request $request): array {
         return [
-            'order_id' => $this->order_id,
-            'discount_id' => $this->discount_id
+            'id'          => $this->id,
+            'order_id'    => $this->order_id,
+            'discount_id' => $this->discount_id,
+            'created_at'  => $this->created_at,
+            'updated_at'  => $this->updated_at,
         ];
     }
 }
