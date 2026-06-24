91e3be488461ab4cdd41947fbab477fb47276bdc
xen <romainrobert.xen@gmail.com>
2026-06-24 16:38:41 +0200

    feat(layout): update components for layout (layout not finished)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,11 +3,12 @@
 <!DOCTYPE html>
 <html lang="fr">
 <x-header />
-<body>
+<body class="bg-[#DEDEDE] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
     <main>
         {{ $slot }}
     </main>
+
 <x-nav />
 </body>
 <x-footer />
