135889e20ea11083ac1432293c7bec79c3aa02ca
xen <romainrobert.xen@gmail.com>
2026-06-25 17:30:38 +0200

    feat(layout): make layout responsive and restructured search bar

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -2,6 +2,6 @@
 
 {{-- fabriqué en bourgogne --}}
 
-<div class="w-full rounded-full flex place-content-center bg-[#1b1b18] text-[#DEDEDE] p-2" >
+<div class="w-full rounded-full flex place-content-center bg-[#1b1b18] text-[#DEDEDE] p-1.5 lg:p-2 text-xs lg:text-sm" >
     fabriqué en bourgogne
 </div>
