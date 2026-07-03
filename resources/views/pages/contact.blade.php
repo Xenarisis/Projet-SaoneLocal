3d634912c9a8a7c8179fa18d5efd233f4c38a2ea
xen <romainrobert.xen@gmail.com>
2026-07-03 14:08:34 +0200

    feat(settings): make setting page and add system dark not dark

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -19,16 +19,16 @@
             <section class="bg-white rounded-2xl p-5 shadow-sm">
                 <h2 class="font-bold text-lg mb-3"> Nous écrire</h2>
                 <p class="text-gray-600">Pour toute question générale :</p>
-                <a href="mailto:contact@saonelocal.fr" class="font-bold text-[#820606] underline">
+                <a href="mailto:contact@saonelocal.fr" class="font-bold text-red-blood underline">
                     contact@saonelocal.fr
                 </a>
             </section>
 
-            <section class="bg-white rounded-2xl p-5 shadow-sm border-l-4 border-[#057941]">
+            <section class="bg-white rounded-2xl p-5 shadow-sm border-l-4 border-base-green">
                 <h2 class="font-bold text-lg mb-3">🌱 Devenir producteur</h2>
                 <p class="text-gray-600 mb-3">Vous êtes producteur local et souhaitez rejoindre notre réseau ? Nous serions ravis de vous accueillir sur la plateforme.</p>
                 <p class="text-gray-600">Contactez-nous directement à :</p>
-                <a href="mailto:producteurs@saonelocal.fr" class="font-bold text-[#057941] underline">
+                <a href="mailto:producteurs@saonelocal.fr" class="font-bold text-base-green underline">
                     producteurs@saonelocal.fr
                 </a>
                 <p class="text-sm text-gray-500 mt-3">Nous vous répondrons dans les meilleurs délais avec toutes les informations nécessaires pour rejoindre SaôneLocal.</p>
--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -1,66 +1,68 @@
 <x-layouts.app title="{{ $product->name }}">
-    <img src="{{ $product->image_path }}" alt="{{ $product->name }}" class="w-full lg:h-248 rounded-2xl m-2">
+    <div class=" bg-white/30 shadow-sm rounded-2xl">
+        <img src="{{ $product->image_path }}" alt="{{ $product->name }}" class="w-full lg:h-248 rounded-2xl m-2">
 
-    <h1 class="lg:text-2xl bg-base-green rounded-2xl text-base-gray text-center">{{ $product->name }}</h1>
+        <h1 class="lg:text-2xl bg-base-green rounded-2xl text-base-gray text-center">{{ $product->name }}</h1>
 
-    <div class="w-full flex justify-center">
-        <button class="border-2 border-black rounded-2xl text-black p-2 mt-4"> Ajouter au panier +</button>
-    </div>
-
-    <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
-        
-        <div @click="open = !open" class="p-4">
-            
-            <p x-show="open">
-                {{ $product->description }}
-            </p>
-            <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
-                <p class="lg:text-2xl rounded-2xl">
-                    Description
-                </p>
-                
-                <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
-                <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
-            </div>
-            
+        <div class="w-full flex justify-center">
+            <button class="border-2 border-black rounded-2xl text-black p-2 mt-4"> Ajouter au panier +</button>
         </div>
-    </div>
 
-    <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
-        
-        <div @click="open = !open" class="p-4">
+        <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
             
-            <p x-show="open">
-                {{-- cards avis --}}
-            </p>
-            <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
-                <p class="lg:text-2xl rounded-2xl">
-                    Avis clients
-                </p>
+            <div @click="open = !open" class="p-4">
                 
-                <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
-                <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
+                <p x-show="open">
+                    {{ $product->description }}
+                </p>
+                <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
+                    <p class="lg:text-2xl rounded-2xl">
+                        Description
+                    </p>
+                    
+                    <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
+                    <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
+                </div>
+                
             </div>
-            
         </div>
-    </div>
 
-    <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
-        
-        <div @click="open = !open" class="p-4">
+        <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
             
-            <p x-show="open">
-                {{-- cards produits --}}
-            </p>
-            <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
-                <p class="lg:text-2xl rounded-2xl">
-                    Autres produits
-                </p>
+            <div @click="open = !open" class="p-4">
                 
-                <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
-                <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
+                <p x-show="open">
+                    {{-- cards avis --}}
+                </p>
+                <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
+                    <p class="lg:text-2xl rounded-2xl">
+                        Avis clients
+                    </p>
+                    
+                    <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
+                    <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
+                </div>
+                
             </div>
+        </div>
+
+        <div x-data="{ open: false }" class="cursor-pointer text-base-gray text-center rounded-2xl m-2">
             
+            <div @click="open = !open" class="p-4">
+                
+                <p x-show="open">
+                    {{-- cards produits --}}
+                </p>
+                <div class=" bg-base-green flex flex-wrap gap-2 justify-center font-bold rounded-2xl p-2">
+                    <p class="lg:text-2xl rounded-2xl">
+                        Autres produits
+                    </p>
+                    
+                    <p x-show="!open" class=" ml-auto lg:text-2xl">></p>
+                    <p x-show="open" class=" ml-auto lg:text-2xl">v</p>
+                </div>
+                
+            </div>
         </div>
     </div>
 </x-layoutsapp>
